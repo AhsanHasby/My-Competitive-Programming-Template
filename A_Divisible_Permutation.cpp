ecc7d5b6065da3b09dac03bf6c00761406446b63
Md Maskaowat Ahsan Hasby <mahahsanhasby@gmail.com>
2026-04-19 22:54:55 +0600

    Problems

---
+++ A_Divisible_Permutation.cpp
@@ -0,0 +1,82 @@
+/*
+██████╗░██╗░██████╗███╗░░░███╗██╗██╗░░░░░██╗░░░░░░█████╗░██╗░░██╗
+██╔══██╗██║██╔════╝████╗░████║██║██║░░░░░██║░░░░░██╔══██╗██║░░██║
+██████╦╝██║╚█████╗░██╔████╔██║██║██║░░░░░██║░░░░░███████║███████║
+██╔══██╗██║░╚═══██╗██║╚██╔╝██║██║██║░░░░░██║░░░░░██╔══██║██╔══██║
+██████╦╝██║██████╔╝██║░╚═╝░██║██║███████╗███████╗██║░░██║██║░░██║
+╚═════╝░╚═╝╚═════╝░╚═╝░░░░░╚═╝╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝*/
+#include<bits/stdc++.h>
+using namespace std;
+
+// Typedefs
+using ll   = long long;
+using ull  = unsigned long long;
+using vin  = vector<int>;
+using vll  = vector<ll>;
+// Constants
+const char endL = '\n';
+const ll   MOD  = (1e9+7); //if given
+// Fast IO
+#define WAKE    ios_base::sync_with_stdio(false);
+#define UP_TO   cin.tie(nullptr);
+#define REALITY cout.tie(nullptr);
+// Macros
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (int)(x.size())
+#define eb       emplace_back //try_emplace(1 insrt)
+
+#define Unique(x) x.erase(unique(all(x)), x.end())
+
+#define print(arr) for(auto &it:(arr)) cout << it << " "; cout << endL;
+#define YES     cout << "YES\n"
+#define NO      cout << "NO\n"
+
+//==========(->MAIN CODE<-)==========
+
+bool UniversalPull() {
+
+    return true; 
+}
+
+void DomainExpansion() {
+    int n; cin >> n;
+    int mid = (n+1)/2;
+    vin p;
+
+    p.eb(mid);
+    if(mid%2 == 0) {
+        p.eb(mid+1);
+        p.eb(mid-1);
+    }
+    else p.eb(midas)
+    for(int d = 1; sz(p) < n; ++d) {
+        if(mid - d >= 1)
+            p.push_back(mid - d);
+        if(sz(p) < n && mid + d <= n)
+            p.push_back(mid + d);
+    }
+
+    for (int x : p) cout << x << " ";
+    cout << endl;
+}
+
+int main() {
+    WAKE UP_TO REALITY
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
+    //test cases
+    int test = 1;
+    cin >> test;
+    //cin.ignore();
+
+    for(int i = 1; i <= test; i++) {
+        //cout << "Case " << i <<": ";
+        DomainExpansion();
+    }
+    return 0;
+}
---
+++ C_Huge_Pile.cpp
@@ -0,0 +1,84 @@
+/*
+██████╗░██╗░██████╗███╗░░░███╗██╗██╗░░░░░██╗░░░░░░█████╗░██╗░░██╗
+██╔══██╗██║██╔════╝████╗░████║██║██║░░░░░██║░░░░░██╔══██╗██║░░██║
+██████╦╝██║╚█████╗░██╔████╔██║██║██║░░░░░██║░░░░░███████║███████║
+██╔══██╗██║░╚═══██╗██║╚██╔╝██║██║██║░░░░░██║░░░░░██╔══██║██╔══██║
+██████╦╝██║██████╔╝██║░╚═╝░██║██║███████╗███████╗██║░░██║██║░░██║
+╚═════╝░╚═╝╚═════╝░╚═╝░░░░░╚═╝╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝*/
+#include<bits/stdc++.h>
+using namespace std;
+
+// Typedefs
+using ll   = long long;
+using ull  = unsigned long long;
+using vin  = vector<int>;
+using vll  = vector<ll>;
+// Constants
+const char endL = '\n';
+const ll   MOD  = (1e9+7); //if given
+// Fast IO
+#define WAKE    ios_base::sync_with_stdio(false);
+#define UP_TO   cin.tie(nullptr);
+#define REALITY cout.tie(nullptr);
+// Macros
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (int)(x.size())
+#define eb       emplace_back //try_emplace(1 insrt)
+
+#define Unique(x) x.erase(unique(all(x)), x.end())
+
+#define print(arr) for(auto &it:(arr)) cout << it << " "; cout << endL;
+#define YES     cout << "YES\n"
+#define NO      cout << "NO\n"
+
+//==========(->MAIN CODE<-)==========
+
+bool UniversalPull() {
+
+    return true; 
+}
+
+void DomainExpansion() {
+    ll n, k; cin >> n >> k;
+    ll cnt = 0;
+    //bool fnd = false;
+
+    if(k > n) {
+        cout << -1 << endL;
+        return;
+    }
+    while(n > k) {
+        ll a = n / 2;
+        ll b = (n+1)/2;
+        if(k <= a) n = a;
+        else if(k <= b) n = b;
+        else break;
+        ++cnt;
+    }
+    if(k == n)
+        cout << cnt << endL;
+    else 
+        cout << -1 << endL;
+    
+}
+
+int main() {
+    WAKE UP_TO REALITY
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
+    //test cases
+    int test = 1;
+    cin >> test;
+    //cin.ignore();
+
+    for(int i = 1; i <= test; i++) {
+        //cout << "Case " << i <<": ";
+        DomainExpansion();
+    }
+    return 0;
+}
